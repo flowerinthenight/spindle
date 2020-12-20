f85b1c00f48b27000510459ce299e8770f3a993b
flowerinthenight <flowerinthenight@pm.me>
2020-12-20 13:28:38 +0900

    rename test

---
+++ examples/simple/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"cloud.google.com/go/spanner"
+	"github.com/flowerinthenight/spindle"
+)
+
+func main() {
+	db, err := spanner.NewClient(
+		context.Background(),
+		"projects/mobingi-main/instances/alphaus-prod/databases/main",
+	)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer db.Close()
+
+	done := make(chan error, 1)
+	quit, cancel := context.WithCancel(context.Background())
+	lock := spindle.New(db, "testlease", "mylock", spindle.WithDuration(5000))
+	lock.Run(quit, done)
+
+	time.Sleep(time.Second * 20)
+	locked, token := lock.HasLock()
+	log.Println(">>>>> HasLock:", locked, token)
+	time.Sleep(time.Second * 20)
+	cancel()
+	<-done
+}
