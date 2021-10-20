18dcb88d3fd19f4cfe7c79026bc52f56f118fd81
flowerinthenight <flowerinthenight@pm.me>
2021-10-20 16:59:49 +0900

    test: trial lock

--- spindle_test.go
+++ spindle_test.go
@@ -3,19 +3,60 @@ package spindle
 import (
 	"context"
 	"testing"
+	"time"
 
 	"cloud.google.com/go/spanner"
+	gaxv2 "github.com/googleapis/gax-go/v2"
 )
 
-func TestInstance(t *testing.T) {
+const (
+	db = "projects/test-project/instances/test-instance/databases/testdb"
+)
+
+func TestLock(t *testing.T) {
 	ctx := context.Background()
-	dbstr := "projects/test-project/instances/test-instance/databases/testdb"
-	client, err := spanner.NewClient(ctx, dbstr)
+	client, err := spanner.NewClient(ctx, db)
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	t.Logf("client=%v", client)
-	client.Close()
+	defer client.Close()
+	done := make(chan error, 1)
+	quit, cancel := context.WithCancel(ctx)
+	lock := New(client, "locktable", "mylock", WithDuration(5000))
+
+	lock.Run(quit, done)
+
+	bo := gaxv2.Backoff{
+		Initial:    time.Second,
+		Max:        time.Second * 30,
+		Multiplier: 2,
+	}
+
+	var cnt int
+	time.Sleep(time.Second * 20)
+
+	for {
+		cnt++
+		locked, token := lock.HasLock()
+		switch {
+		case locked:
+			t.Logf("lock obtained, token=%v", token)
+			break
+		default:
+			t.Log("lock not obtained, retry")
+			time.Sleep(bo.Pause())
+			continue
+		}
+
+		if cnt >= 10 {
+			t.Fatalf("can't get lock")
+		}
+
+		break
+	}
+
+	cancel()
+	<-done
 }
