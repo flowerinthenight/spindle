b333b6a3cba9045a255ff508f15b4d4fd016f9c5
flowerinthenight <flowerinthenight@pm.me>
2021-10-20 16:11:13 +0900

    test: trial emulator

---
+++ spindle_test.go
@@ -0,0 +1,21 @@
+package spindle
+
+import (
+	"context"
+	"testing"
+
+	"cloud.google.com/go/spanner"
+)
+
+func TestInstance(t *testing.T) {
+	ctx := context.Background()
+	dbstr := "projects/test-project/instances/test-instance/databases/db"
+	client, err := spanner.NewClient(ctx, dbstr)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Logf("client=%v", client)
+	client.Close()
+}
