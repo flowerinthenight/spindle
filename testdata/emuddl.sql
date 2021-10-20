fc5e9c793873dd7ba9989f75a76d74ef9fb811ba
flowerinthenight <flowerinthenight@pm.me>
2021-10-20 18:00:06 +0900

    test: use ddlfile

---
+++ testdata/emuddl.sql
@@ -0,0 +1,7 @@
+-- for spindle
+CREATE TABLE locktable (
+    name STRING(MAX) NOT NULL,
+    heartbeat TIMESTAMP OPTIONS (allow_commit_timestamp=true),
+    token TIMESTAMP OPTIONS (allow_commit_timestamp=true),
+    writer STRING(MAX)
+) PRIMARY KEY (name)
