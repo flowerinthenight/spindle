392d83f02f8b014b80bd9a95832a129948410045
flowerinthenight <flowerinthenight@pm.me>
2023-01-10 17:46:03 +0900

    chore: revert cleanup, doesn't work

--- lock/lock.go
+++ lock/lock.go
@@ -43,7 +43,6 @@ func NewSpindleLock(opts *SpindleLockOptions) *SpindleLock {
 		sopts = append(sopts, spindle.WithLogger(opts.Logger))
 	}
 
-	sopts = append(sopts, spindle.WithCleanRecordsOnExit(true))
 	s.lock = spindle.New(opts.Client, opts.Table, opts.Name, sopts...)
 	return s
 }
